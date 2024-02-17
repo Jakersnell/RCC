db29a8685d802c52af3b656dc2dd3bdbe7d1fe7e
Jacob Snell <jake.r.snell@gmail.com>
2024-02-16 17:30:07 -0700

    Remove experimental tests

--- src/main.rs
+++ src/main.rs
@@ -13,4 +13,14 @@ mod str_intern;
 mod tokens;
 
 fn main() {
+    let x = "sd";
+    macro_rules! matching {
+        ($($pattern:literal)|+) => {
+            match x {
+                $($pattern)|+ => println!("Matched"),
+                _ => println!("Not matched"),
+            }
+        };
+    }
+    matching!("sdd" | "sdf");
 }
