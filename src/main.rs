416931f710240526da883a3c6e321d4e7ce50351
Jacob Snell <jake.r.snell@gmail.com>
2024-02-19 16:45:06 -0700

    Remove test data

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,11 @@
 #![allow(unused)]
 
-use std::{
-    collections::{hash_map::DefaultHasher, HashSet},
-    hash::{BuildHasherDefault, Hasher},
-    sync::Arc,
-};
-
 mod ast;
 mod error;
-mod file;
 mod lex;
 mod parse;
 mod str_intern;
 mod tokens;
+mod util;
 
-fn main() {
-    let x = "sd";
-    macro_rules! matching {
-        ($($pattern:literal)|+) => {
-            match x {
-                $($pattern)|+ => println!("Matched"),
-                _ => println!("Not matched"),
-            }
-        };
-    }
-    matching!("sdd" | "sdf");
-}
+fn main() {}
