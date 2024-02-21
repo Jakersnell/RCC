fc1e9424a88a554565165064c0257334d6229fac
Jacob Snell <jake.r.snell@gmail.com>
2024-02-21 10:03:39 -0700

    Remove tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 #![allow(unused)]
 
-use crate::ast::pretty_print_ast;
-use crate::tokens::Symbol;
-
 mod ast;
 mod error;
 mod lex;
@@ -15,4 +12,18 @@ mod util;
 /// This will be the last to be completed as all the other components of the program
 /// need to function for the main to be usable.
 /// Cant compile a program if you don't have a lexer or parser.
-fn main() {}
+fn main() {
+    let program = util::Program::new("main.c".to_string());
+    let source = "
+2 + 4 * 5 - 6 / 3;
+8+3;
+"
+    .to_string();
+    let lexer = lex::Lexer::new(source);
+    let mut parser = parse::Parser::from_lexer(program, lexer);
+    let program = parser.parse();
+    let ast = program.body.unwrap().unwrap();
+    for node in ast {
+        print!("{}", node);
+    }
+}
