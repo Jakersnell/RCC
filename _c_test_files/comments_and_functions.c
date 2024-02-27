be0b430c30058903232f379b21d1dc9f99533570
Jacob Snell <jake.r.snell@gmail.com>
2024-02-26 17:23:32 -0700

    Remove gitignore of _c_test_files

---
+++ _c_test_files/comments_and_functions.c
@@ -0,0 +1,17 @@
+// int x = 1 + 4 / 2 + y * 20; this is in a comment
+/*
+double y = 2.3434; this is also in a comment, it will be ignored by the lexer
+*/
+
+double take_and_return_double(double d) {
+    int new_double = d * 2 + 1;
+    return d;
+}
+
+int x = 4;
+double y;
+
+int main() {
+    y = y + x;
+    return 0;
+}
---
+++ _c_test_files/const_ptr.c
@@ -0,0 +1,6 @@
+
+
+const int *const myTest;
+int main() {
+    return 0;
+}
