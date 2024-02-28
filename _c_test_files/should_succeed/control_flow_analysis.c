68223bf21bf8fc086b037e73d25f391f98d17687
Jacob Snell <jake.r.snell@gmail.com>
2024-02-28 16:08:03 -0700

    add fuzzing, and test files

---
+++ _c_test_files/should_succeed/control_flow_analysis.c
@@ -0,0 +1,15 @@
+int test(int a, int b) {
+    if (a > b) {
+        return 1;
+    } else if (a == b) {
+        return 0;
+    } else {
+        return -1;
+    }
+
+}
+
+int main() {
+    test(1, 2);
+    return 0;
+}
---
+++ _c_test_files/should_succeed/fuzz.c
@@ -0,0 +1,62 @@
+// fuzz.c
+/*
+This file is a fuzzing test for the compiler, fuzzing is essentially throwing random input
+at the compiler to see if it crashes. This is a good way to find bugs and other issues.
+all the code in this file is semantically correct, and should compile. if it doesnt then there
+is a bug in the compiler.
+*/
+
+const double PI = 3.14159;
+
+struct test {
+    int a;
+    int b;
+    double f;
+};
+
+void foo(struct test *a, int *b) {
+    a->a = *b;
+}
+
+const int x = {1};
+
+int main() {
+    int y;
+    int x = sizeof(y);
+    char c = 'a';
+    char *p = &c;
+    double d = c++ * *p + 4 + 4;
+    struct test t;
+    t.a = 4;
+    t.b = 5;
+    t.b++;
+    ;;;;;;;
+    t.a = sizeof(struct test);
+    struct test *pt = &t;
+    pt->a = 4;
+    pt->b = 5;
+    if (pt->a == 4) {
+        pt->b = 6;
+    }
+    if (pt->b == 5) {
+        pt->a = 5;
+    } else {
+        pt->a = 6;
+    }
+    while (pt->a < 10) {
+        pt->a++;
+    }
+    for (int i = 0; i < 10; i++) {
+        pt->a++;
+    }
+    int i;
+    for (i = 0; i < 10; i++) {
+        pt->a++;
+    }
+    for (;;) {
+        pt->a++;
+    }
+    int x[] = {1, 2, 3, 4, 5};
+    foo(pt, &i);
+    return 0;
+}
