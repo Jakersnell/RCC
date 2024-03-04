845665f39242f3a8e7cf8a5e25601da20f2ddc7f
Jacob Snell <jake.r.snell@gmail.com>
2024-03-04 10:59:51 -0700

    add trait ErrorReporter and basic implementing struct

--- src/error.rs
+++ src/error.rs
@@ -1,16 +1,67 @@
 use crate::util::Span;
 use thiserror::Error;
 
+pub trait ErrorReporter: Default + Sized {
+    fn get_status(&self) -> Result<(), ()>;
+    fn report_error(&mut self, error: CompilerError);
+    fn report_warning(&mut self, warning: CompilerWarning);
+    fn get_errors(&self) -> &Vec<CompilerError>;
+    fn get_warnings(&self) -> &Vec<CompilerWarning>;
+}
+
+pub enum ProgramErrorStatus {
+    MustStop, // must stop immediately
+    Current,  // can continue this phase but not next
+    Lexer,
+    Parser,
+    Analysis,
+    Codegen,
+}
+
+#[derive(Default)]
+pub struct ErrorReporterImpl {
+    errors: Vec<CompilerError>,
+    warnings: Vec<CompilerWarning>,
+}
+
+impl ErrorReporter for ErrorReporterImpl {
+    fn get_status(&self) -> Result<(), ()> {
+        if self.errors.is_empty() {
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
+    fn report_error(&mut self, error: CompilerError) {
+        println!("{}", error);
+        self.errors.push(error);
+    }
+
+    fn report_warning(&mut self, warning: CompilerWarning) {
+        println!("{}", warning);
+        self.warnings.push(warning);
+    }
+
+    fn get_errors(&self) -> &Vec<CompilerError> {
+        &self.errors
+    }
+
+    fn get_warnings(&self) -> &Vec<CompilerWarning> {
+        &self.warnings
+    }
+}
+
 #[derive(Error, Debug)]
 pub enum CompilerError {
     #[error("{0}")]
     IoError(#[from] std::io::Error),
 
-    #[error("{0}")]
-    ParseIntError(#[from] std::num::ParseIntError),
+    #[error("Invalid integer literal: {0}")]
+    ParseIntError(Span),
 
-    #[error("{0}")]
-    ParseFloatError(#[from] std::num::ParseFloatError),
+    #[error("Invalid float literal: {0}")]
+    ParseFloatError(Span),
 
     #[error("Invalid integer suffix: {0}")]
     InvalidIntegerSuffix(String, Span),
@@ -82,7 +133,7 @@ pub enum CompilerError {
     IdentifierExists(Span),
 
     #[error("{0}")]
-    CustomError(String),
+    CustomError(String, Span),
 
     #[error("Else without if")]
     ElseWithNoIf(Span),
@@ -91,26 +142,23 @@ pub enum CompilerError {
 #[derive(Error, Debug)]
 pub enum CompilerWarning {
     #[error("Unused variable: {0}")]
-    UnusedVariable(String),
+    UnusedVariable(Span),
 
     #[error("Unused function: {0}")]
-    UnusedFunction(String),
+    UnusedFunction(Span),
 
     #[error("Unused parameter: {0}")]
-    UnusedParameter(String),
+    UnusedParameter(Span),
 
     #[error("Unused constant: {0}")]
-    UnusedConstant(String),
+    UnusedConstant(Span),
 
     #[error("Unused struct: {0}")]
-    UnusedStruct(String),
-
-    #[error("Unused enum: {0}")]
-    UnusedEnum(String),
-
-    #[error("Unused union: {0}")]
-    UnusedUnion(String),
+    UnusedStruct(Span),
 
     #[error("Unreachable code: {0}")]
-    UnreachableCode(String),
+    UnreachableCode(Span),
+
+    #[error("Variable is not initialized at this point: {0}")]
+    UninitializedVariable(Span),
 }
