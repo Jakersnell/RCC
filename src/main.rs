79549dd1193320da31c7569ee17286a8f3a3cb53
Jacob Snell <jake.r.snell@gmail.com>
2024-03-06 12:15:56 -0700

    implement integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused)]
 
-use crate::util::error::{CompilerError, CompilerWarning, ErrorReporter};
+use crate::util::error::{CompilerError, CompilerWarning};
 use std::cell::RefCell;
 use std::path::PathBuf;
 use std::rc::Rc;
@@ -15,85 +15,23 @@ mod util;
 /// requires the API to be complete in order to function.
 /// Cant compile a program if you don't have a compiler.
 
-const PRETTY: bool = false;
-
 fn main() {}
 
 #[cfg(test)]
 mod tests {
     use crate::parser::ast::InitDeclaration;
     use crate::parser::ParseResult;
-    use crate::util::error::{CompilerError, CompilerWarning, ErrorReporter};
+    use crate::util::error::{CompilerError, CompilerWarning};
     use crate::util::{CompilerResult, Locatable};
     use crate::{lexer, parser};
     use std::cell::RefCell;
     use std::rc::Rc;
-    struct MockReporter {
-        errors: Vec<CompilerError>,
-        warnings: Vec<CompilerWarning>,
-        status: Result<(), ()>,
-    }
-
-    impl MockReporter {
-        fn new() -> Self {
-            Self {
-                errors: Vec::new(),
-                warnings: Vec::new(),
-                status: Ok(()),
-            }
-        }
-    }
-
-    impl ErrorReporter for MockReporter {
-        fn get_status(&self) -> Result<(), ()> {
-            self.status
-        }
-
-        fn report_error(&mut self, error: CompilerError) {
-            self.errors.push(error);
-            self.status = Err(());
-        }
-
-        fn report_warning(&mut self, warning: CompilerWarning) {
-            self.warnings.push(warning);
-        }
-
-        fn get_errors(&self) -> &Vec<CompilerError> {
-            &self.errors
-        }
-
-        fn get_warnings(&self) -> &Vec<CompilerWarning> {
-            &self.warnings
-        }
-    }
-
-    #[test]
-    fn remove_this() {
-        let source = "
-        int main() {
-            int x = 0;
-            return 0;
-        }
-        ";
-        let reporter = Rc::new(RefCell::new(MockReporter::new()));
-        let lexer = lexer::Lexer::new(reporter.clone(), source.into());
-        let tokens = lexer.collect::<Vec<_>>();
-        println!("{:#?}", tokens);
-        // let mut parser = parser::Parser::new(reporter.clone(), Box::new(lexer));
-        // let results = parser
-        //     .parse_all()
-        //     .map_err(|_| std::mem::take(&mut reporter.borrow_mut().errors));
-        // println!("{:#?}", results);
-    }
 
     fn run_test_on_file(path: &str) -> Result<Vec<Locatable<InitDeclaration>>, Vec<CompilerError>> {
         let source = std::fs::read_to_string(path).expect("Could not read file.");
-        let reporter = Rc::new(RefCell::new(MockReporter::new()));
-        let lexer = lexer::Lexer::new(reporter.clone(), source.into());
-        let parser = parser::Parser::new(reporter.clone(), Box::new(lexer));
-        parser
-            .parse_all()
-            .map_err(|_| std::mem::take(&mut reporter.borrow_mut().errors))
+        let lexer = lexer::Lexer::new(source.into());
+        let parser = parser::Parser::new(lexer);
+        parser.parse_all()
     }
 
     #[test]
