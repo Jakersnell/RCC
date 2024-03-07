ab41d2b9c04855bfa45799e918a68523a837a038
Jacob Snell <jake.r.snell@gmail.com>
2024-03-06 18:30:23 -0700

    add symbol resolver and HLIR, create flow module

--- src/analysis/mod.rs
+++ src/analysis/mod.rs
@@ -1,76 +1,25 @@
-mod hlir;
+mod flow;
+pub mod hlir;
+mod symbols;
 
-use crate::parser::ast::ASTRoot;
+use crate::analysis::hlir::HighLevelIR;
+use crate::analysis::symbols::SymbolResolver;
+use crate::parser::ast::{ASTRoot, AbstractSyntaxTree};
+use crate::util::error::CompilerError;
 use crate::util::str_intern::InternedStr;
-use crate::util::CompilerResult;
-use hlir::{BoundTypeKind, SymbolKind};
+use crate::util::Locatable;
 use std::collections::HashMap;
 
 pub struct Validator<'a> {
-    current_resolver: SymbolResolver<'a>,
-    root: ASTRoot,
-}
-
-pub struct SymbolResolver<'a> {
-    symbols: HashMap<InternedStr, SymbolKind>,
-    parent: Option<&'a SymbolResolver<'a>>,
-}
-
-impl<'a> SymbolResolver<'a> {
-    pub fn new(parent: Option<&'a SymbolResolver<'_>>) -> Self {
-        Self {
-            symbols: HashMap::new(),
-            parent,
-        }
-    }
-
-    #[inline]
-    pub fn ident_exists_in_scope(&self, ident: &InternedStr) -> bool {
-        self.symbols.contains_key(ident)
-    }
-
-    #[inline]
-    fn add_symbol(&mut self, ident: InternedStr, kind: SymbolKind) -> CompilerResult<()> {
-        todo!()
-    }
-
-    #[inline]
-    pub fn add_function(
-        &mut self,
-        ident: InternedStr,
-        ty: BoundTypeKind,
-        parameters: Vec<BoundTypeKind>,
-    ) -> CompilerResult<()> {
-        self.add_symbol(ident, SymbolKind::Function { ty, parameters })
-    }
-
-    #[inline]
-    pub fn add_variable(&mut self, ident: InternedStr, ty: BoundTypeKind) -> CompilerResult<()> {
-        self.add_symbol(ident, SymbolKind::Variable(ty))
-    }
-}
-
-struct ControlFlowAnalyzer {
-    // ...
-}
-
-struct ControlNode {
-    // ...
-}
-
-struct ControlEdge {
-    // ...
+    hlir: HighLevelIR,
+    global: SymbolResolver<'a>,
+    ast: AbstractSyntaxTree,
 }
 
 impl<'a> Validator<'a> {
-    pub fn new(root: ASTRoot) -> Self {
-        let root_resolver = SymbolResolver {
-            symbols: HashMap::new(),
-            parent: None,
-        };
-        Self {
-            current_resolver: root_resolver,
-            root,
-        }
+    pub fn new(ast: AbstractSyntaxTree) -> Self {
+        let global = SymbolResolver::create_root();
+        let hlir = HighLevelIR::default();
+        Self { hlir, global, ast }
     }
 }
