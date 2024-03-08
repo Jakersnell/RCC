74526b9fd36aa24f41ac7f09e40a26f59aec27dc
Jacob Snell <jake.r.snell@gmail.com>
2024-03-08 11:11:01 -0700

    complete symbol resolver

--- src/analysis/mod.rs
+++ src/analysis/mod.rs
@@ -2,24 +2,258 @@ mod flow;
 pub mod hlir;
 mod symbols;
 
-use crate::analysis::hlir::HighLevelIR;
+use crate::analysis::hlir::{HighLevelIR, HlirType, HlirTypeKind};
 use crate::analysis::symbols::SymbolResolver;
-use crate::parser::ast::{ASTRoot, AbstractSyntaxTree};
-use crate::util::error::CompilerError;
+use crate::parser::ast::{
+    ASTRoot, AbstractSyntaxTree, Block, DeclarationSpecifier, InitDeclaration, TypeQualifier,
+    TypeSpecifier, VariableDeclaration,
+};
+use crate::util::error::{CompilerError, CompilerWarning, Reporter};
 use crate::util::str_intern::InternedStr;
-use crate::util::Locatable;
+use crate::util::{Locatable, Span};
+use derive_new::new;
+use std::cell::RefCell;
 use std::collections::HashMap;
+use std::rc::Rc;
 
-pub struct Validator<'a> {
-    hlir: HighLevelIR,
-    global: SymbolResolver<'a>,
+pub type SharedReporter = Rc<RefCell<Reporter>>;
+
+pub struct GlobalValidator<'a> {
     ast: AbstractSyntaxTree,
+    scope: SymbolResolver<'a>,
+    reporter: SharedReporter,
 }
 
-impl<'a> Validator<'a> {
+impl<'a> GlobalValidator<'a> {
     pub fn new(ast: AbstractSyntaxTree) -> Self {
-        let global = SymbolResolver::create_root();
+        Self {
+            ast,
+            scope: SymbolResolver::create_root(),
+            reporter: Rc::new(RefCell::new(Reporter::default())),
+        }
+    }
+
+    fn report_error(&mut self, error: CompilerError) {
+        self.reporter.borrow_mut().report_error(error);
+    }
+
+    fn report_warning(&mut self, warning: CompilerWarning) {
+        self.reporter.borrow_mut().report_warning(warning);
+    }
+
+    pub fn validate(mut self) -> Result<HighLevelIR, Reporter> {
         let hlir = HighLevelIR::default();
-        Self { hlir, global, ast }
+        for node in &*self.ast {
+            use crate::parser::ast::InitDeclaration::*;
+            match node {
+                Declaration(locatable_variable) => {}
+                Function(locatable_function) => {}
+                Struct(locatable_struct) => {}
+            }
+        }
+        todo!("validation")
+    }
+
+    fn validate_variable(
+        &mut self,
+        locatable_variable: &Locatable<VariableDeclaration>,
+    ) -> Result<(), ()> {
+        let span = locatable_variable.location;
+        let var = &locatable_variable.value;
+
+        // validate declaration for the variable
+        // the declaration must have an ident.
+        // validate the declaration specifier
+        // for now all storage specifiers are ignored, report a warning displaying this
+        // the only type qualifier is const, if we see it, it is counted, anything after is redundant
+        // type specifier feels like it may be similar to a finite state machine
+        // if we see signed or unsigned we expect one of the following after, char, int, long,
+        // anything else is an error
+        // anything coming after a type otherwise is an error, except for Long, because of Long long
+
+        // if is array it will probably need a size
+        // if is array with no size the size must be calculated via the assignment,
+        // and thus requires an assignment
+
+        let declaration = &var.declaration;
+        if declaration.value.ident.is_none() {
+            let err = CompilerError::DeclarationMissingIdentifier(declaration.location);
+            self.report_error(err);
+            return Err(());
+        }
+
+        let ident = declaration.value.ident.as_ref().unwrap();
+        let ident_span = ident.location;
+        let ident = ident.value.clone();
+
+        let specifier = &declaration.specifier;
+        let specifier_span = specifier.location;
+        let specifier = &specifier.value;
+
+        for storage_spec in &specifier.specifiers {
+            // need to change this span to the specific storage spec location
+            self.report_warning(CompilerWarning::UnsupportedStorageSpecifier(
+                storage_spec.to_string(),
+                specifier_span,
+            ))
+        }
+
+        let mut is_const = false;
+        for ty_qual in &specifier.qualifiers {
+            // this is set up for expansion
+            match ty_qual {
+                TypeQualifier::Const => {
+                    if is_const {
+                        let warning =
+                            CompilerWarning::RedundantUsage(ty_qual.to_string(), specifier_span);
+                        self.report_warning(warning);
+                    } else {
+                        is_const = true;
+                    }
+                }
+            }
+        }
+        let is_const = is_const; // remove mutability
+
+        Ok(())
+    }
+
+    fn validate_type_specifiers(
+        &mut self,
+        specifiers: &[TypeSpecifier],
+        location: Span,
+    ) -> Result<HlirType, ()> {
+        enum State {
+            Start,
+            SeenUnsigned,
+            SeenUnsignedLong,
+            SeenSigned,
+            SeenSignedLong,
+            End,
+        }
+        let mut hlir_type: Option<HlirTypeKind> = None;
+        let mut state = State::Start;
+        let mut iter = specifiers.iter();
+        loop {
+            let ty_spec = iter.next();
+            match state {
+                State::Start => match ty_spec {
+                    Some(TypeSpecifier::Void) => {
+                        hlir_type = Some(HlirTypeKind::Void);
+                        state = State::End;
+                    }
+                    Some(TypeSpecifier::Char) => {
+                        hlir_type = Some(HlirTypeKind::Char(false));
+                        state = State::End;
+                    }
+                    Some(TypeSpecifier::Int) => {
+                        hlir_type = Some(HlirTypeKind::Int(false));
+                        state = State::End;
+                    }
+                    Some(TypeSpecifier::Long) => {
+                        state = State::SeenSignedLong;
+                    }
+                    Some(TypeSpecifier::Double) => {
+                        hlir_type = Some(HlirTypeKind::Double);
+                        state = State::End;
+                    }
+                    Some(TypeSpecifier::Struct(ident)) => {
+                        hlir_type = Some(HlirTypeKind::Struct(ident.clone()));
+                        state = State::End;
+                    }
+                    Some(TypeSpecifier::Signed) => {
+                        state = State::SeenSigned;
+                    }
+                    Some(TypeSpecifier::Unsigned) => {
+                        state = State::SeenUnsigned;
+                    }
+                    None => panic!(
+                        "Fatal compiler error: Missing type specifier at state start. src-location: {:?}",
+                        location
+                    ),
+                },
+                State::SeenUnsigned => match ty_spec {
+                    Some(TypeSpecifier::Char) => {
+                        hlir_type = Some(HlirTypeKind::Char(true));
+                        state = State::End;
+                    }
+                    Some(TypeSpecifier::Int) => {
+                        hlir_type = Some(HlirTypeKind::Int(true));
+                        state = State::End;
+                    }
+                    Some(TypeSpecifier::Long) => {
+                        state = State::SeenUnsignedLong;
+                    }
+                    Some(ty) => {
+                        let err = CompilerError::TypeCannotBeSignedOrUnsigned(
+                            ty.to_string(),
+                            location,
+                        );
+                        self.report_error(err);
+                        return Err(());
+                    }
+                    None => {
+                        let err = CompilerError::ExpectedTypeSpecifier(location);
+                        self.report_error(err);
+                        return Err(());
+                    }
+
+                },
+                State::SeenUnsignedLong => match ty_spec {
+                    Some(TypeSpecifier::Int) => {
+                        hlir_type = Some(HlirTypeKind::Long(true));
+                        state = State::End;
+                    }
+                    Some(TypeSpecifier::Long) => {
+                        hlir_type = Some(HlirTypeKind::LLong(true));
+                        state = State::End;
+                    }
+                    Some(ty) => {
+                            let err = CompilerError::TypeCannotBeSignedOrUnsigned(
+                                ty.to_string(),
+                                location,
+                            );
+                            self.report_error(err);
+                        return Err(());
+                    }
+                    None => {
+                        hlir_type = Some(HlirTypeKind::Long(true));
+                        state = State::End;
+                    }
+                },
+                State::SeenSigned => match ty_spec {
+                    Some(TypeSpecifier::Char) => {
+                        hlir_type = Some(HlirTypeKind::Char(false));
+                        state = State::End;
+                    }
+                    Some(TypeSpecifier::Int) => {
+                        hlir_type = Some(HlirTypeKind::Int(false));
+                        state = State::End;
+                    }
+                    Some(TypeSpecifier::Long) => {
+                        state = State::SeenSignedLong;
+                    }
+                    Some(ty) => {
+                        let err = CompilerError::TypeCannotBeSignedOrUnsigned(
+                            ty.to_string(),
+                            location,
+                        );
+                        self.report_error(err);
+                        return Err(());
+                    }
+                    None => {
+                        let err = CompilerError::ExpectedTypeSpecifier(location);
+                        self.report_error(err);
+                        return Err(());
+                    }
+                },
+                State::SeenSignedLong => {}
+                State::End => {
+                    debug_assert!(hlir_type.is_some());
+                    break;
+                }
+            }
+        }
+        todo!()
     }
 }
--- src/analysis/symbols.rs
+++ src/analysis/symbols.rs
@@ -1,10 +1,10 @@
-use crate::analysis::hlir::{HlirType, HlirTypeKind};
+use crate::analysis::hlir::{HlirType, HlirTypeKind, HlirVariable};
 use crate::util::error::CompilerError;
 use crate::util::str_intern::InternedStr;
 use crate::util::{str_intern, Locatable, Span};
 use std::collections::HashMap;
 
-pub type SymbolResult<T> = Result<T, CompilerError>;
+pub type SymbolResult = Result<(), CompilerError>;
 #[derive(Debug, Clone)]
 pub struct BuiltinFunctionSymbol {
     ty: &'static str,
@@ -70,12 +70,18 @@ thread_local! {
 pub enum SymbolKind {
     BuiltinFunction(BuiltinFunctionSymbol),
     UserFunction(UserFunctionSymbol),
-    Variable(HlirType),
+    Variable(VariableSymbol),
 }
 
 pub struct UserFunctionSymbol {
     return_ty: HlirType,
     parameters: Vec<HlirType>,
+}
+
+pub struct VariableSymbol {
+    ty: HlirType,
+    is_const: bool,
+    is_initialized: bool,
 }
 
 pub struct SymbolResolver<'a> {
@@ -114,99 +120,117 @@ impl<'a> SymbolResolver<'a> {
     #[inline]
     pub fn add_function(
         &mut self,
-        ident: &Locatable<InternedStr>,
+        ident: &InternedStr,
         return_ty: HlirType,
         parameters: Vec<HlirType>,
-    ) -> SymbolResult<()> {
-        self.add_symbol(
-            ident,
-            SymbolKind::UserFunction(UserFunctionSymbol {
-                return_ty,
-                parameters,
-            }),
-        )
+        span: Span,
+    ) -> SymbolResult {
+        let symbol = SymbolKind::UserFunction(UserFunctionSymbol {
+            return_ty,
+            parameters,
+        });
+        self.add_symbol(ident, symbol, span)
     }
 
     #[inline]
     pub fn add_variable(
         &mut self,
-        ident: &Locatable<InternedStr>,
-        ty: HlirType,
-    ) -> SymbolResult<()> {
-        self.add_symbol(ident, SymbolKind::Variable(ty))
+        ident: &InternedStr,
+        ty: &HlirType,
+        is_const: bool,
+        is_initialized: bool,
+        span: Span,
+    ) -> SymbolResult {
+        let symbol = SymbolKind::Variable(VariableSymbol {
+            ty: ty.clone(),
+            is_const,
+            is_initialized,
+        });
+        self.add_symbol(ident, symbol, span)
     }
 
     #[inline]
-    fn add_symbol(&mut self, ident: &Locatable<InternedStr>, kind: SymbolKind) -> SymbolResult<()> {
-        if self.symbols.contains_key(&ident.value) {
-            Err(CompilerError::IdentifierExists(ident.location))
+    fn add_symbol(&mut self, ident: &InternedStr, kind: SymbolKind, span: Span) -> SymbolResult {
+        if self.symbols.contains_key(ident) {
+            Err(CompilerError::IdentifierExists(span))
         } else {
-            self.symbols.insert(ident.value.clone(), kind);
+            self.symbols.insert(ident.clone(), kind);
             Ok(())
         }
     }
 
-    pub fn validate_function_call(
+    pub fn get_function_call(
         &self,
         ident: &InternedStr,
         args: &[HlirType],
         span: Span,
-    ) -> SymbolResult<()> {
+    ) -> Result<HlirType, CompilerError> {
         let kind = self.retrieve(ident, span)?;
-        fn validate(
-            varargs: bool,
-            params: &[HlirType],
-            args: &[HlirType],
-            span: Span,
-        ) -> SymbolResult<()> {
-            if varargs {
-                Ok(())
-            } else if params.len() == args.len() {
-                for (param, arg) in params.iter().zip(args.iter()) {
-                    if param != arg {
-                        return Err(CompilerError::FunctionTypeMismatch(span));
-                    }
-                }
-                Ok(())
-            } else {
-                Err(CompilerError::FunctionTypeMismatch(span))
-            }
-        }
         match kind {
             SymbolKind::BuiltinFunction(builtin) => {
-                validate(builtin.varargs, &builtin.params, args, span)
+                Self::validate_function_params(builtin.varargs, &builtin.params, args, span)?;
+                Ok(builtin.return_ty.clone())
             }
-            SymbolKind::UserFunction(user) => validate(false, &user.parameters, args, span),
+            SymbolKind::UserFunction(user) => {
+                Self::validate_function_params(false, &user.parameters, args, span)?;
+                Ok(user.return_ty.clone())
+            }
             SymbolKind::Variable(_) => Err(CompilerError::NotAFunction(span)),
         }
     }
 
-    fn validate_variable_type(
+    fn validate_function_params(
+        varargs: bool,
+        params: &[HlirType],
+        args: &[HlirType],
+        span: Span,
+    ) -> SymbolResult {
+        if varargs {
+            return Ok(());
+        }
+        if params.len() != args.len() {
+            return Err(CompilerError::FunctionTypeMismatch(span));
+        }
+        for (param, arg) in params.iter().zip(args.iter()) {
+            if param != arg {
+                return Err(CompilerError::FunctionTypeMismatch(span));
+            }
+        }
+        Ok(())
+    }
+
+    pub fn get_variable_type(
         &self,
         ident: &InternedStr,
-        ty: &HlirType,
         span: Span,
-    ) -> SymbolResult<()> {
-        let kind = self.retrieve(ident, span)?;
-        match kind {
-            SymbolKind::Variable(var_ty) => {
-                if var_ty != ty {
-                    Err(CompilerError::VariableTypeMismatch(
-                        span,
-                        ty.clone(),
-                        var_ty.clone(),
-                    ))
-                } else {
-                    Ok(())
-                }
-            }
+    ) -> Result<HlirType, CompilerError> {
+        match self.retrieve(ident, span)? {
+            SymbolKind::Variable(var_ty) => Ok(var_ty.ty.clone()),
             SymbolKind::BuiltinFunction(_) | SymbolKind::UserFunction(_) => {
                 Err(CompilerError::NotAVariable(span))
             }
         }
     }
 
-    fn retrieve(&self, ident: &InternedStr, span: Span) -> SymbolResult<&SymbolKind> {
+    pub fn check_valid_assignment(
+        &self,
+        ident: &InternedStr,
+        ty: &HlirType,
+        span: Span,
+    ) -> SymbolResult {
+        let var_ty = self.get_variable_type(ident, span)?;
+        if var_ty != *ty {
+            Err(CompilerError::VariableTypeMismatch(
+                span,
+                ty.to_string(),
+                var_ty.to_string(),
+            ))
+        } else {
+            Ok(())
+        }
+    }
+
+    fn retrieve(&self, ident: &InternedStr, span: Span) -> Result<&SymbolKind, CompilerError> {
         let symbol = self.symbols.get(ident);
         if let Some(symbol) = symbol {
             Ok(symbol)
