4f829c0fa371762effe817fbbd02d29f97190da3
Jacob Snell <jake.r.snell@gmail.com>
2024-03-15 13:12:52 -0600

    add arch.rs

---
+++ src/util/arch.rs
@@ -0,0 +1,16 @@
+pub const LONG_SIZE: u32 = 8;
+pub const LONG_MAX: i64 = i64::MAX;
+pub const LONG_MIN: i64 = i64::MIN;
+pub const UNSIGNED_LONG_MAX: u64 = u64::MAX;
+pub const FLOAT_SIZE: u32 = 4;
+pub const DOUBLE_SIZE: u32 = 8;
+pub const CHAR_SIZE: u32 = 1;
+pub const CHAR_MAX: i8 = i8::MAX;
+pub const CHAR_MIN: i8 = i8::MIN;
+pub const UNSIGNED_CHAR_MAX: u8 = u8::MAX;
+pub const INT_SIZE: u32 = 4;
+pub const INT_MAX: i32 = i32::MAX;
+pub const INT_MIN: i32 = i32::MIN;
+pub const UNSIGNED_INT_MAX: u32 = u32::MAX;
+
+pub const POINTER_SIZE: u32 = 8;
