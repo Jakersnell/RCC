1011c04b7b44e08eb3211613f92adbf2550f8ba3
Jacob Snell <jake.r.snell@gmail.com>
2024-03-15 17:14:10 -0600

    homogenize const types

--- src/util/arch.rs
+++ src/util/arch.rs
@@ -1,16 +1,19 @@
-pub const LONG_SIZE: u32 = 8;
+pub const CHAR_SIZE: u64 = 1;
+pub const CHAR_MAX: u64 = i8::MAX as u64;
+pub const CHAR_MIN: u64 = i8::MIN as u64;
+pub const UNSIGNED_CHAR_MAX: u64 = u8::MAX as u64;
+
+pub const LONG_SIZE: u64 = 8;
 pub const LONG_MAX: i64 = i64::MAX;
 pub const LONG_MIN: i64 = i64::MIN;
 pub const UNSIGNED_LONG_MAX: u64 = u64::MAX;
-pub const FLOAT_SIZE: u32 = 4;
-pub const DOUBLE_SIZE: u32 = 8;
-pub const CHAR_SIZE: u32 = 1;
-pub const CHAR_MAX: i8 = i8::MAX;
-pub const CHAR_MIN: i8 = i8::MIN;
-pub const UNSIGNED_CHAR_MAX: u8 = u8::MAX;
-pub const INT_SIZE: u32 = 4;
-pub const INT_MAX: i32 = i32::MAX;
-pub const INT_MIN: i32 = i32::MIN;
-pub const UNSIGNED_INT_MAX: u32 = u32::MAX;
 
-pub const POINTER_SIZE: u32 = 8;
+pub const INT_SIZE: u64 = 4;
+pub const INT_MAX: i64 = i32::MAX as i64;
+pub const INT_MIN: i64 = i32::MIN as i64;
+pub const UNSIGNED_INT_MAX: u64 = u32::MAX as u64;
+
+pub const FLOAT_SIZE: u64 = 4;
+pub const DOUBLE_SIZE: u64 = 8;
+
+pub const POINTER_SIZE: u64 = 8;
