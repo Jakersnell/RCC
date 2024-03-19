f01cf68cb1a21a5f3e52fea7de06b3e6f276e5bc
Jacob Snell <jake.r.snell@gmail.com>
2024-03-18 18:00:03 -0600

    add get_file_paths to test module

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,28 @@ fn main() {}
 
 #[cfg(test)]
 mod tests {
-    use crate::parser::ast::InitDeclaration;
+    use crate::parser::ast::{Expression, InitDeclaration};
     use crate::parser::ParseResult;
     use crate::util::error::{CompilerError, CompilerWarning};
     use crate::util::{CompilerResult, Locatable};
     use crate::{lexer, parser};
     use std::cell::RefCell;
+    use std::env::var;
+    use std::path::PathBuf;
     use std::rc::Rc;
 
-    fn run_test_on_file(path: &str) -> Result<Vec<Locatable<InitDeclaration>>, Vec<CompilerError>> {
+    fn get_file_paths(path: &PathBuf) -> std::io::Result<Vec<PathBuf>> {
+        let mut paths = Vec::new();
+        let res_paths = std::fs::read_dir(path)?.map(|res| res.map(|entry| entry.path()));
+        for path in res_paths {
+            paths.push(path?);
+        }
+        Ok(paths)
+    }
+
+    fn run_test_on_file(
+        path: &PathBuf,
+    ) -> Result<Vec<Locatable<InitDeclaration>>, Vec<CompilerError>> {
         let source = std::fs::read_to_string(path).expect("Could not read file.");
         let lexer = lexer::Lexer::new(source.into());
         let parser = parser::Parser::new(lexer);
@@ -36,43 +49,55 @@ mod tests {
 
     #[test]
     fn test_should_succeed_files() {
-        let tests = [
-            "structs.c",
-            "comments_and_functions.c",
-            "sizeof.c",
-            "const_ptr.c",
-            "member_access.c",
-            "fuzz.c",
-            "control_flow_analysis.c",
-        ];
+        let tests = get_file_paths(&"_c_test_files/should_succeed".into()).unwrap();
 
         for test in tests {
-            let result = run_test_on_file(&format!("_c_test_files/should_succeed/{}", test));
+            let result = run_test_on_file(&test);
             match result {
                 Ok(body) => {
-                    assert!(!body.is_empty(), "No body in file: {}", test);
+                    assert!(!body.is_empty(), "No body in file: {:#?}", test);
                 }
-                Err(errors) => panic!("File {} errors:\n{:#?}", test, errors),
+                Err(errors) => panic!("File {:#?} errors:\n{:#?}", test, errors),
             }
         }
     }
 
     #[test]
     fn test_should_fail_files() {
-        let tests = [
-            "declaration_in_expression.c",
-            "function_in_struct.c",
-            "if_in_condition.c",
-        ];
+        let tests = get_file_paths(&"_c_test_files/should_fail".into()).unwrap();
 
         for test in tests {
-            let result = run_test_on_file(&format!("_c_test_files/should_fail/{}", test));
+            let result = run_test_on_file(&test);
             match result {
                 Err(errors) => {
-                    assert!(!errors.is_empty(), "No errors in file: {}", test);
+                    assert!(!errors.is_empty(), "No errors in file: {:#?}", test);
                 }
-                Ok(body) => panic!("File {} succeeded:\n{:#?}", test, body),
+                Ok(body) => panic!("File {:#?} succeeded", test),
             }
         }
+    }
+
+    #[test]
+    fn test_subscripting_order() {
+        let src = "int y = &x[3];";
+        let lexer = lexer::Lexer::new(src.into());
+        let parser = parser::Parser::new(lexer);
+        let result = parser.parse_all().unwrap();
+        let var_dec = &**result.first().expect("Expected non-empty result.");
+
+        let variable = match var_dec {
+            InitDeclaration::Declaration(variable) => &**variable,
+            _ => panic!("First element should be a variable declaration!"),
+        };
+
+        let init = &**variable
+            .initializer
+            .as_ref()
+            .expect("Expected initializer in variable declaration.");
+
+        match init {
+            Expression::Unary(crate::parser::ast::UnaryOp::AddressOf, _) => (),
+            _ => panic!("Incorrect Parse Tree: Expected unary operator AddressOf '&'"),
+        };
     }
 }
