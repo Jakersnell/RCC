e3f109204d2c886ac9aebd1db0919d0ca022ad2a
Jacob Snell <jake.r.snell@gmail.com>
2024-05-27 12:48:18 -0600

    complete 'compile_assignment'

--- src/codegen/expressions/binary_expressions.rs
+++ src/codegen/expressions/binary_expressions.rs
@@ -1,7 +1,9 @@
-use inkwell::values::BasicValueEnum;
+use inkwell::types::BasicTypeEnum;
+use inkwell::values::{BasicValueEnum, PointerValue};
 
 use crate::codegen::Compiler;
-use crate::data::mlir::MlirExpr;
+use crate::data::mlir::{MlirExpr, MlirExprKind};
+use crate::util::str_intern::InternedStr;
 
 impl<'a, 'mlir, 'ctx> Compiler<'a, 'mlir, 'ctx> {
     pub(super) fn compile_assignment(
@@ -9,6 +11,52 @@ impl<'a, 'mlir, 'ctx> Compiler<'a, 'mlir, 'ctx> {
         left: &MlirExpr,
         right: &MlirExpr,
     ) -> BasicValueEnum<'ctx> {
-        todo!()
+        debug_assert!(left.is_lval);
+        let assign_value = self.compile_expression(right);
+
+        let assign_ptr = match &*left.kind {
+            MlirExprKind::Variable(id) => self.get_pointer(id),
+            MlirExprKind::Deref(expr) => self.compile_expression(expr).into_pointer_value(),
+            MlirExprKind::Member(_struct, member) => {
+                self.get_struct_member_pointer(self.convert_type(&left.ty), _struct, member)
+            }
+            MlirExprKind::Index(array, index) => {
+                self.get_array_index_pointer(self.convert_type(&left.ty), array, index)
+            }
+            _ => self.compile_expression(left).into_pointer_value(),
+        };
+
+        self.builder().build_store(assign_ptr, assign_value);
+
+        assign_value
+    }
+
+    fn get_struct_member_pointer(
+        &mut self,
+        access_ty: BasicTypeEnum<'ctx>,
+        _struct: &'mlir MlirExpr,
+        member: &InternedStr,
+    ) -> PointerValue<'ctx> {
+        let struct_ident = _struct.ty.kind.get_struct_ident();
+        let _struct = self.compile_expression(_struct).into_pointer_value();
+        let member_offset = self.mlir.get_struct_member_offset(&struct_ident, member);
+        self.builder()
+            .build_struct_gep(access_ty, _struct, member_offset, "get_struct_member_ptr")
+            .unwrap()
+    }
+
+    fn get_array_index_pointer(
+        &mut self,
+        access_ty: BasicTypeEnum<'ctx>,
+        array: &'mlir MlirExpr,
+        index: &'mlir MlirExpr,
+    ) -> PointerValue<'ctx> {
+        let array_ptr = self.compile_expression(array).into_pointer_value();
+        let index_value = self.compile_expression(index).into_int_value();
+        unsafe {
+            self.builder()
+                .build_gep(access_ty, array_ptr, &[index_value], "get_array_index_ptr")
+                .unwrap()
+        }
     }
 }
