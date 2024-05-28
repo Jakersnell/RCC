8846e27fb72b4a0c436041550922928ab04bf88c
Jacob Snell <jake.r.snell@gmail.com>
2024-05-28 13:54:08 -0600

    create comparison and 'compile_logical_and' compilation functions

--- src/codegen/expressions/binary_expressions.rs
+++ src/codegen/expressions/binary_expressions.rs
@@ -1,3 +1,4 @@
+use inkwell::{FloatPredicate, IntPredicate};
 use inkwell::types::BasicTypeEnum;
 use inkwell::values::{BasicValueEnum, PointerValue};
 
@@ -5,18 +6,50 @@ use crate::codegen::Compiler;
 use crate::data::mlir::{MlirExpr, MlirExprKind};
 use crate::util::str_intern::InternedStr;
 
+enum CmpOp {
+    Eq,
+    Ne,
+    Lt,
+    Gt,
+    Lte,
+    Gte,
+}
 macro_rules! build_arithmetic_binop {
     (
         $compiler:ident, $left:ident, $right:ident;
-        $int_case:ident;
+        $signed_int_case:ident;
         $float_case:ident;
     ) => {
+        build_arithmetic_binop!(
+            $compiler, $left, $right;
+            $signed_int_case;
+            $signed_int_case;
+            $float_case;
+            false;
+        )
+    };
+
+    (
+        $compiler:ident, $left:ident, $right:ident;
+        $signed_int_case:ident;
+        $unsigned_int_case:ident;
+        $float_case:ident;
+        $unsigned_int: expr;
+    ) => {
         match $compiler.compile_binary_expr($left, $right) {
-            (BasicValueEnum::IntValue(left), BasicValueEnum::IntValue(right)) => {
+            (BasicValueEnum::IntValue(left), BasicValueEnum::IntValue(right)) if !$unsigned_int => {
                 BasicValueEnum::from(
                     $compiler
                         .builder()
-                        .$int_case(left, right, stringify!($int_case))
+                        .$signed_int_case(left, right, stringify!($int_case))
+                        .unwrap(),
+                )
+            }
+            (BasicValueEnum::IntValue(left), BasicValueEnum::IntValue(right)) if $unsigned_int => {
+                BasicValueEnum::from(
+                    $compiler
+                        .builder()
+                        .$unsigned_int_case(left, right, stringify!($int_case))
                         .unwrap(),
                 )
             }
@@ -151,11 +184,179 @@ impl<'a, 'mlir, 'ctx> Compiler<'a, 'mlir, 'ctx> {
         &mut self,
         left: &MlirExpr,
         right: &MlirExpr,
+        unsigned_int: bool,
     ) -> BasicValueEnum<'ctx> {
         build_arithmetic_binop!(
             self, left, right;
             build_int_signed_div;
+            build_int_unsigned_div;
             build_float_div;
+            unsigned_int;
         )
+    }
+
+    pub(super) fn compile_modulus(
+        &mut self,
+        left: &MlirExpr,
+        right: &MlirExpr,
+        unsigned_int: bool,
+    ) -> BasicValueEnum<'ctx> {
+        build_arithmetic_binop!(
+            self, left, right;
+            build_int_signed_rem;
+            build_int_unsigned_rem;
+            build_float_rem;
+            unsigned_int;
+        )
+    }
+
+    fn build_comparison(
+        &mut self,
+        left: &MlirExpr,
+        right: &MlirExpr,
+        int_predicate: IntPredicate,
+        float_predicate: FloatPredicate,
+    ) -> BasicValueEnum<'ctx> {
+        match self.compile_binary_expr(left, right) {
+            (BasicValueEnum::IntValue(left), BasicValueEnum::IntValue(right)) => {
+                BasicValueEnum::from(
+                    self.builder()
+                        .build_int_compare(int_predicate, left, right, "int_eq")
+                        .unwrap(),
+                )
+            }
+            (BasicValueEnum::FloatValue(left), BasicValueEnum::FloatValue(right)) => {
+                BasicValueEnum::from(
+                    self.builder()
+                        .build_float_compare(float_predicate, left, right, "float_eq")
+                        .unwrap(),
+                )
+            }
+            unexpected => panic!(
+                "Expected (int, int) or (float, float) but found '{:?}'",
+                unexpected
+            ),
+        }
+    }
+
+    #[inline(always)]
+    pub(super) fn compile_equal(
+        &mut self,
+        left: &MlirExpr,
+        right: &MlirExpr,
+    ) -> BasicValueEnum<'ctx> {
+        self.build_comparison(left, right, IntPredicate::EQ, FloatPredicate::OEQ)
+    }
+
+    #[inline(always)]
+    pub(super) fn compile_not_equal(
+        &mut self,
+        left: &MlirExpr,
+        right: &MlirExpr,
+    ) -> BasicValueEnum<'ctx> {
+        self.build_comparison(left, right, IntPredicate::NE, FloatPredicate::ONE)
+    }
+
+    #[inline(always)]
+    pub(super) fn compile_greater_than(
+        &mut self,
+        left: &MlirExpr,
+        right: &MlirExpr,
+        unsigned_int: bool,
+    ) -> BasicValueEnum<'ctx> {
+        self.build_comparison(
+            left,
+            right,
+            if unsigned_int {
+                IntPredicate::UGT
+            } else {
+                IntPredicate::SGT
+            },
+            FloatPredicate::OGT,
+        )
+    }
+
+    #[inline(always)]
+    pub(super) fn compile_greater_than_equal(
+        &mut self,
+        left: &MlirExpr,
+        right: &MlirExpr,
+        unsigned_int: bool,
+    ) -> BasicValueEnum<'ctx> {
+        self.build_comparison(
+            left,
+            right,
+            if unsigned_int {
+                IntPredicate::UGE
+            } else {
+                IntPredicate::SGE
+            },
+            FloatPredicate::OGE,
+        )
+    }
+
+    #[inline(always)]
+    pub(super) fn compile_less_than(
+        &mut self,
+        left: &MlirExpr,
+        right: &MlirExpr,
+        unsigned_int: bool,
+    ) -> BasicValueEnum<'ctx> {
+        self.build_comparison(
+            left,
+            right,
+            if unsigned_int {
+                IntPredicate::ULT
+            } else {
+                IntPredicate::SLT
+            },
+            FloatPredicate::OLT,
+        )
+    }
+
+    #[inline(always)]
+    pub(super) fn compile_less_than_equal(
+        &mut self,
+        left: &MlirExpr,
+        right: &MlirExpr,
+        unsigned_int: bool,
+    ) -> BasicValueEnum<'ctx> {
+        self.build_comparison(
+            left,
+            right,
+            if unsigned_int {
+                IntPredicate::ULE
+            } else {
+                IntPredicate::SLE
+            },
+            FloatPredicate::OLE,
+        )
+    }
+
+    pub(super) fn compile_logical_and(
+        &mut self,
+        left: &MlirExpr,
+        right: &MlirExpr,
+    ) -> BasicValueEnum<'ctx> {
+        let left = self.compile_expression(left).into_int_value();
+        let right = self.compile_expression(right).into_int_value();
+        let zero = self.context.i8_type().const_int(0, false);
+
+        let logical_left = self
+            .builder()
+            .build_int_compare(IntPredicate::NE, left, zero, "logical_cmp_left")
+            .unwrap();
+
+        let logical_right = self
+            .builder()
+            .build_int_compare(IntPredicate::NE, right, zero, "logical_cmp_right")
+            .unwrap();
+
+        let value = self
+            .builder()
+            .build_and(logical_left, logical_right, "logical_and")
+            .unwrap();
+
+        BasicValueEnum::from(value)
     }
 }
