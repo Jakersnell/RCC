d963fb3293ec9075b9d40686c78f5a17a15c3f2d
Jacob Snell <jake.r.snell@gmail.com>
2024-05-31 19:50:28 -0600

    add compile_variable_declaration

--- src/codegen/declarations.rs
+++ src/codegen/declarations.rs
@@ -1,60 +1,64 @@
-use inkwell::types::{BasicType, BasicTypeEnum};
-use inkwell::values::{BasicValueEnum, PointerValue};
+use inkwell::types::BasicTypeEnum;
+use inkwell::values::PointerValue;
 
 use crate::codegen::Compiler;
-use crate::data::mlir::{MlirExpr, MlirVarInit, MlirVariable};
+use crate::data::mlir::{MlirExpr, MlirTypeDecl, MlirVariable, MlirVarInit};
 
 impl<'a, 'mlir, 'ctx> Compiler<'a, 'mlir, 'ctx> {
-    pub(super) fn compile_variable_declaration(&mut self, var: &MlirVariable) {
+    pub fn compile_variable_declaration(&mut self, var: &MlirVariable) {
         let MlirVariable {
             uid,
             span,
-            ty,
+            ty: mlir_type,
             ident,
             is_const,
             initializer,
         } = var;
-        let ty = self.convert_type(ty);
-        let var_ptr = self.create_entry_block_allocation(ty, &uid.to_string());
+
+        let (ty, var_ptr) = match &mlir_type.decl {
+            MlirTypeDecl::Array(size) => {
+                let element_type = self.convert_type(&mlir_type.as_basic());
+                (
+                    element_type,
+                    self.create_entry_block_array_allocation(element_type, *size),
+                )
+            }
+            _ => {
+                let ty = self.convert_type(mlir_type);
+                (ty, self.create_entry_block_allocation(ty, ident))
+            }
+        };
+
         self.variables.insert(*uid, var_ptr).unwrap();
 
-        let initializer = match initializer.as_ref().map(|init| &init.value) {
-            Some(MlirVarInit::Array(array)) => self.compile_array_initializer(ty, array),
-            Some(MlirVarInit::Expr(expr)) => self.compile_expression(expr),
-            None => todo!(),
-        };
+        if let Some(initializer) = initializer {
+            match &initializer.value {
+                MlirVarInit::Array(array) => {
+                    debug_assert!(ty.is_array_type());
+                    self.compile_array_initializer(var_ptr, ty, array);
+                }
+                MlirVarInit::Expr(expr) => {
+                    let expr = self.compile_expression(expr);
+                    self.builder().build_store(var_ptr, expr).unwrap();
+                }
+            }
+        }
     }
 
     fn compile_array_initializer(
         &mut self,
-        ty: BasicTypeEnum<'ctx>,
+        ptr: PointerValue<'ctx>,
+        element_type: BasicTypeEnum<'ctx>,
         array: &[MlirExpr],
-    ) -> BasicValueEnum<'ctx> {
-        let array = array
+    ) {
+        let elements = array
             .iter()
             .map(|expr| self.compile_expression(expr))
             .collect::<Vec<_>>();
-        let count = array.len();
-
-        todo!()
-    }
-
-    fn create_array_allocation(
-        &mut self,
-        ty: BasicTypeEnum<'ctx>,
-        size: usize,
-        name: &str,
-    ) -> PointerValue<'ctx> {
-        let builder = self.context.create_builder();
-        let entry = self.fn_value().get_first_basic_block().unwrap();
-        let array_type = ty.array_type(size as u32);
-        todo!()
-    }
-
-    fn create_basic_default_initializer(
-        &mut self,
-        ty: BasicTypeEnum<'ctx>,
-    ) -> BasicValueEnum<'ctx> {
-        todo!()
+        for (idx, e) in elements.iter().enumerate() {
+            let idx = self.context.i32_type().const_int(idx as u64, false);
+            let ptr = self.get_array_index_pointer(element_type, ptr, idx);
+            self.builder().build_store(ptr, *e).unwrap();
+        }
     }
 }
