f3e729fd11db8b040198f053af7bfea1dcc05949
Jacob Snell <jake.r.snell@gmail.com>
2024-06-01 11:48:00 -0600

    remove unconditional branch building upon block having no jump, moved that to mod

--- src/codegen/statements.rs
+++ src/codegen/statements.rs
@@ -7,7 +7,7 @@ use crate::util::str_intern::InternedStr;
 impl<'a, 'mlir, 'ctx> Compiler<'a, 'mlir, 'ctx> {
     pub fn compile_mlir_basic_block(&mut self, mlir_bb: &MlirBasicBlock) {
         for stmt in &mlir_bb.stmts {
-            let mut block_has_jumped = false;
+            self.block_has_jumped = false;
             match stmt {
                 MlirStmt::Expression(expression) => {
                     self.compile_expression_statement(expression);
@@ -18,32 +18,22 @@ impl<'a, 'mlir, 'ctx> Compiler<'a, 'mlir, 'ctx> {
                 }
 
                 MlirStmt::Goto(label) => {
-                    block_has_jumped = true;
+                    self.block_has_jumped = true;
                     self.compile_goto(label);
                 }
 
                 MlirStmt::CondGoto(condition, then, _else) => {
-                    block_has_jumped = true;
+                    self.block_has_jumped = true;
                     self.compile_cond_goto(condition, then, _else);
                 }
 
                 MlirStmt::Return(expression) => {
-                    block_has_jumped = true;
+                    self.block_has_jumped = true;
                     self.compile_return_statement(expression);
                 }
 
                 MlirStmt::Block(_) | MlirStmt::Label(_) => {
                     unreachable!("Blocks and labels must not exist at this stage in the process.")
-                }
-            }
-            if !block_has_jumped {
-                let next_block_opt = self
-                    .builder()
-                    .get_insert_block()
-                    .unwrap()
-                    .get_next_basic_block();
-                if let Some(next_block) = next_block_opt {
-                    self.builder().build_unconditional_branch(next_block);
                 }
             }
         }
@@ -60,7 +50,7 @@ impl<'a, 'mlir, 'ctx> Compiler<'a, 'mlir, 'ctx> {
     }
 
     #[inline(always)]
-    fn compile_goto(&self, label: &InternedStr) {
+    fn compile_goto(&mut self, label: &InternedStr) {
         let goto_block = self.get_block_by_name(label).unwrap();
         self.builder().build_unconditional_branch(goto_block);
     }
