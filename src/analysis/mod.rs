4c597fbb1d64a734510642d3a6d7637d3ef93ba9
Jacob Snell <jake.r.snell@gmail.com>
2024-08-22 18:43:53 -0600

    Modify mod

--- src/analysis/mod.rs
+++ src/analysis/mod.rs
@@ -4,13 +4,13 @@ use std::iter::Map;
 use std::ops::{Deref, DerefMut};
 use std::rc::Rc;
 
+use crate::{display_mlir, output_analyzer};
 use crate::analysis::symbols::SymbolResolver;
 use crate::data::ast::*;
 use crate::data::error::{CompilerError, CompilerWarning, Reporter};
 use crate::data::mlir::*;
-use crate::util::str_intern::InternedStr;
 use crate::util::{Locatable, Span};
-use crate::{display_mlir, output_analyzer};
+use crate::util::str_intern::InternedStr;
 
 mod binary_expressions;
 mod casting;
@@ -20,6 +20,7 @@ mod expressions;
 mod statements;
 mod symbols;
 
+// Is used to keep track of how many MlirStmt::Label are made
 static mut LABEL_COUNT: usize = 0;
 
 pub(in crate::analysis) fn peek_current_label() -> usize {
@@ -408,9 +409,9 @@ fn test_validate_type_returns_ok_for_valid_type_orientations() {
 
 #[cfg(test)]
 mod tests {
+    use crate::{analysis, lexer, parser};
     use crate::analysis::SharedReporter;
     use crate::data::mlir::MlirModule;
-    use crate::{analysis, lexer, parser};
 
     pub(in crate::analysis) fn run_analysis_test(path: &str) -> Result<MlirModule, SharedReporter> {
         let source = std::fs::read_to_string(path).expect("Could not read file.");
